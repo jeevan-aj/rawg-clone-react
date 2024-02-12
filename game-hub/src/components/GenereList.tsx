46ac5025e3a63328e09c6a8da754ad5913b6bada
jeevan-aj <ambrahamjeevan@gmail.com>
2024-02-12 15:18:56 +0530

    show genre skeleton

--- game-hub/src/components/GenereList.tsx
+++ game-hub/src/components/GenereList.tsx
@@ -1,15 +1,44 @@
-import useGenere from "../hooks/useGenere"
-
+import {
+  HStack,
+  Image,
+  List,
+  ListItem,
+  Skeleton,
+  SkeletonCircle,
+  Text,
+} from "@chakra-ui/react";
+import useGenere from "../hooks/useGenere";
+import crop from "../services/image-url";
 
 function GenereList() {
-    const {data} = useGenere()
+  const { data, loading,error } = useGenere();
+  const genereSkeleton = [1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 1, 1];
+  if(error) return null;
   return (
-    <ul>
-        {data.map(cur => (
-            <li key={cur.id}>{cur.name}</li>
+    <List px={5} mt={7}>
+      {loading &&
+        genereSkeleton.map(() => (
+          <HStack py={2}>
+            <SkeletonCircle></SkeletonCircle>
+            <Skeleton height={3} width={"80px"}>
+              d
+            </Skeleton>
+          </HStack>
         ))}
-    </ul>
-  )
+      {data.map((cur) => (
+        <ListItem key={cur.id} paddingY={2} fontSize="lg">
+          <HStack>
+            <Image
+              boxSize={"32px"}
+              borderRadius={8}
+              src={crop(cur.image_background)}
+            ></Image>
+            <Text>{cur.name}</Text>
+          </HStack>
+        </ListItem>
+      ))}
+    </List>
+  );
 }
 
-export default GenereList
+export default GenereList;
